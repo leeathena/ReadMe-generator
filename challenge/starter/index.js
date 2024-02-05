45424c2199a43ba7589e04a38509e1fc21752b66
leeathena <hoyan.la@gmail.com>
2024-02-06 01:36:13 +0800

    completed JS codes

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const util = require('util');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
@@ -21,17 +22,17 @@ const questions = [
         message: 'What is your table of content? List them in bullet points.'
     },
     {
-        type: 'Input',
+        type: 'input',
         name:'installation',
         message: 'How should the user install your project locally?'
     },
     {
-        type: 'Input',
+        type: 'input',
         name:'usage',
         message: 'How to use your project after the users installed it?'
     },
     {
-        type: 'Input',
+        type: 'input',
         name:'Contributing',
         message: 'Do you have any specific contribution preferences?'
     },
@@ -45,22 +46,38 @@ const questions = [
     ]
 ;    
 
-const writeFileAsync = until.promisify(fs.writeFile);
+
 
 // function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>{
-        if (err){
-            return console.error(`There is an error occurred while writing to ${answers.title}`, err);``
+function sanitizeTitle(title) {
+    return title.replace(/[^a-zA-Z0-9]/g, '_'); // Replace non-alphanumeric characters with underscores
+}
+
+function writeToFile(fileName, data, title) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.error(`There was an error occurred while writing to ${title}:`, err);
         }
-        console.log (`${answers.title} was created sucessfully`)
+        console.log(`${title} was created successfully`);
     });
 }
 
-// function to initialize program
-function init() {
 
+// function to initialize program
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const markdownContent = generateMarkdown(answers);
+        const sanitizedTitle = sanitizeTitle(answers.title);
+        const fileName = `${sanitizedTitle}.md`; // Create a filename based on the title
+        writeToFile(fileName, markdownContent, answers.title);
+    } catch (error) {
+        console.error('Failed to generate README:', error);
+    }
 }
+
+
+
 
 // function call to initialize program
 init();
